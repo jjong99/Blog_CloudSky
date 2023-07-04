0c283a174a1a884b12387528151dd756626e51e6
Suin <tktw555@gmail.com>
2023-07-04 09:44:02 +0900

    작성한 글 목록에 아이디가 아닌 닉네임으로 표시되도록 수정

--- src/main/java/com/example/cloudsky/dto/ProfilePostListResponseDto.java
+++ src/main/java/com/example/cloudsky/dto/ProfilePostListResponseDto.java
@@ -7,13 +7,13 @@ import java.time.LocalDateTime;
 
 @Getter
 public class ProfilePostListResponseDto {
-    private String username; // 사용자의 닉네임으로 표시하기
+    private String realname; // 사용자의 닉네임으로 표시하기
     private String title;
     private LocalDateTime createdAt;
 
     public ProfilePostListResponseDto(Post post) {
         this.title = post.getTitle();
-        this.username = post.getUser().getUsername();
+        this.realname = post.getUser().getRealname();
         this.createdAt = post.getCreatedAt();
     }
 }
